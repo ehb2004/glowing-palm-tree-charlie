334a6f07ab361e18b1c10cb39ef7ef4059eb8bae
ehb2004 <149546842+ehb2004@users.noreply.github.com>
2025-02-25 16:35:12 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// doc ready plus an easter egg
+$( document ).ready(function() {
+    console.log( "ready!" );
+    console.log("YOu shOuld'NT bE hEre");
+    console.log("Seriously, being nosy is rude");
+    console.log("I'm not mad, just disappointed");
+});
+
